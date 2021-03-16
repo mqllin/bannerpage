f62ccb9d12f3416553043a46e1cd843bb4146937
Lin <584363361@qq.com>
2021-03-16 12:45:19 +0800

    左侧菜单

---
+++ app/src/main/java/com/example/bannerpage/Activity/normal/DrawActivity.java
@@ -0,0 +1,16 @@
+package com.example.bannerpage.Activity.normal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bannerpage.R;
+
+public class DrawActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw);
+    }
+}
