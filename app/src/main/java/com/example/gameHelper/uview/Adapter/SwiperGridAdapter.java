bec7779d93c319f1eaebab83fc83614a4e59ddc3
Lin <584363361@qq.com>
2021-03-26 08:41:14 +0800

    u

---
+++ app/src/main/java/com/example/gameHelper/uview/Adapter/SwiperGridAdapter.java
@@ -0,0 +1,37 @@
+package com.example.gameHelper.uview.Adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import java.util.List;
+
+public class SwiperGridAdapter extends PagerAdapter {
+    public List<View> views;
+    public SwiperGridAdapter(List<View> views){
+        this.views = views;
+    }
+    @Override
+    public int getCount() {
+        return views.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view==object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        container.addView(views.get(position),0);
+        return views.get(position);
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView(views.get(position));
+    }
+}
--- app/src/main/java/com/example/gameHelper/uview/Components/MyGridView/MySwiperGrid.java
+++ app/src/main/java/com/example/gameHelper/uview/Components/MyGridView/MySwiperGrid.java
@@ -7,10 +7,13 @@ import android.widget.GridView;
 import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.gameHelper.uview.Adapter.DefaultGridAdapter;
 import com.example.gameHelper.R;
+import com.example.gameHelper.uview.Adapter.SwiperGridAdapter;
 import com.example.gameHelper.uview.Struct.GridViewItem;
+import com.example.gameHelper.uview.Struct.GridViewList;
 
 import java.util.List;
 
@@ -18,6 +21,8 @@ import java.util.List;
 public class MySwiperGrid extends LinearLayout {
     private Context mContext;
     private GridView gridView;
+    private List<GridView> gridViewList;
+    private ViewPager swiper;
 
     public MySwiperGrid(Context context) {
         super(context);
@@ -35,14 +40,51 @@ public class MySwiperGrid extends LinearLayout {
     /**初始化swiper九宫格
      * @param list 数据集合
      * @param columns 一行显示几列数据，推荐5
+     * @param rows 显示几行数据，推荐2
      * @param padding 设置item之间的间距，推荐10*/
-    public void init(List<GridViewItem> list, int columns, int padding){
+    public void init(List<GridViewItem> list, int columns,int rows, int padding){
         LayoutInflater.from(mContext).inflate(R.layout.my_grid_view,this);
-        DefaultGridAdapter gridAdapter = new DefaultGridAdapter(mContext, R.layout.grid_view_item,list);
-        gridView = findViewById(R.id.c_grid_view);
-        gridView.setNumColumns(columns);
-        gridView.setVerticalSpacing(padding);
-        gridView.setAdapter(gridAdapter);
+        List<List<GridViewItem>> itemGroup = null; //总数据
+        List<GridViewItem> itemList = null;//临时存储每一页的数据
+        int pageSize = columns * rows; // 一页显示多少个item
+        int current_index=0;//当前页数
+
+
+//        分组
+        for(int i=0,size=list.size();i<size;i++){
+            if(itemGroup.size()<pageSize){
+                itemList.add(list.get(i));
+            }
+            if(itemList.size()==pageSize){
+                itemGroup.add(itemList);
+                current_index=current_index+1;
+                itemList = null;
+            }
+        }
+
+
+        for(int i=0,size = itemGroup.size();i<size;i++){
+            DefaultGridAdapter gridAdapter = new DefaultGridAdapter(mContext, R.layout.grid_view_item,itemGroup.get(i));
+            gridView = findViewById(R.id.c_grid_view);
+            gridView.setNumColumns(columns);
+            gridView.setVerticalSpacing(padding);
+            gridView.setAdapter(gridAdapter);
+            gridViewList.add(gridView);
+
+        }
+
+
+
+//
+//        LayoutInflater.from(mContext).inflate(R.layout.my_swiper_grid_view,this);
+//        SwiperGridAdapter swiperGridAdapter = new SwiperGridAdapter(mContext, R.layout.my_swiper_grid_view,list);
+//        swiper = findViewById(R.id.c_swiper_grid_view);
+//        swiper.setAdapter(swiperGridAdapter);
+
+
+
+
+
     }
 
 }
